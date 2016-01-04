71d76c23804d3451785a541354877cbee05e810b
Jamen Marzonie <jamenmarz@gmail.com>
2016-01-04 09:56:04 -0700

    Add uuid dep

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import Server from './server';
+import pack from '../package';
+
+export default {
+  // Objects
+  Server,
+
+  // Methods
+  // ...
+
+  // Meta
+  author: pack.author,
+  license: pack.license,
+  version: pack.version,
+};
