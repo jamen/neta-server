6ddfe38cc7c4fdb98c05bab1e696ccf04e02fd74
Jamen Marzonie <jamenmarz@gmail.com>
2016-01-04 10:18:05 -0700

    Better test

--- test/server.js
+++ test/server.js
@@ -1,7 +1,19 @@
+/* eslint no-console: 0 */
+
 import test from 'ava';
 import Server from '../out';
 
 
 test.cb('Server class', t => {
-  
+  const server = new Server();
+
+  server.on('message', (message, socket) => {
+    console.log(socket.id, ':', message);
+  });
+
+  server.on('close', () => {
+    t.end();
+  });
+
+  server.listen(1337);
 });
