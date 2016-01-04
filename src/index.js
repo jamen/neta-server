b3302478d0c9b20836b5c04ae88b5cd238484af9
Jamen Marzonie <jamenmarz@gmail.com>
2016-01-04 09:53:23 -0700

    Add basic server.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
+import net from 'net';
+import uuid from 'uuid';
+import { EventEmitter } from 'events';
+
 /**
  * Server class
  */
+class Server extends EventEmitter {
+  /**
+   * Constructor
+   * @param {Server} server - previously initialized server.
+   */
+  constructor(server) {
+    super();
+    this.sockets = [];
+    this._server = server || new net.Server();
 
-class Server {
+    this._server.on('connection', socket => {
+      socket.id = uuid.v1();
+      this.sockets.push(socket);
+      this.emit('connection', socket);
 
-};
+      socket.on('data', data => {
+        this.emit('message', data.toString().trim(), socket);
+      });
+    });
+  }
+
+  /**
+   * Broadcast a message.
+   * @param {(String|Buffer)} message - something to broadcast.
+   * @return {Self} Chainability.
+   */
+  broadcast(message) {
+    this.sockets(s => s.write(message));
+    return this;
+  }
+
+  /**
+   * Listen
+   */
+  listen(...opts) {
+    this._server.listen(...opts);
+  }
+}
 
 export default Server;
